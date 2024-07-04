9d292709579f8c8b435e6b3179b5286a7bd3164a
Nguyen Nghia <ngd.nghia28@gmail.com>
2024-07-04 16:09:33 +0700

    init project

---
+++ src/index.ts
@@ -0,0 +1,72 @@
+import { SuiClient, getFullnodeUrl } from '@mysten/sui/client';
+import { MIST_PER_SUI } from '@mysten/sui/utils';
+import { program } from 'commander';
+import { createTxsendSUITo, fromBech32File, generatePairAndSave, myParseInt, parseBalance, sleep } from "./utils";
+import { getFaucetHost, requestSuiFromFaucetV1 } from '@mysten/sui/faucet';
+
+(async () => {
+    program
+        .command("transfer")
+        .description("transfer to address")
+        .action(transfer)
+        .requiredOption('-a, --address <string>', 'Source account to send SUI')
+        .requiredOption('-n, --count <number>', 'Number of accounts to send', myParseInt)
+        .requiredOption('-v, --value <number>', 'Value to sent to each account in SUI', myParseInt)
+        .option('-N, --network <string>', 'Network to work with', 'devnet');
+    program.command('balance')
+        .description("get balance")
+        .argument('<string>', 'address to query balance')
+        .option('-N, --network <string>', 'Network to work with', 'devnet')
+        .action(balance);
+    program.command('faucet')
+        .description("faucet")
+        .argument('<string>', 'address to faucet')
+        .option('-N, --network <string>', 'Network to work with', 'devnet')
+        .action(faucet)
+
+    program.parse(process.argv)
+})()
+
+async function balance(address: string, { network }: { network: "mainnet" | "testnet" | "devnet" | "localnet" }) {
+    console.log(`Get balance for ${address}`);
+
+    const suiClient = new SuiClient({ url: getFullnodeUrl(network) });
+    console.log(`Current balance: ${parseBalance(await suiClient.getBalance({ owner: address }))}`)
+}
+
+async function faucet(address: string, { network }: { network: "mainnet" | "testnet" | "devnet" | "localnet" }) {
+    console.log(`Faucet for ${address}`);
+
+    const suiClient = new SuiClient({ url: getFullnodeUrl(network) });
+    if (network === 'mainnet') {
+        throw new Error('Cannot faucet from mainnet')
+    }
+
+    console.log(`Current balance: ${parseBalance(await suiClient.getBalance({ owner: address }))}`)
+    await requestSuiFromFaucetV1({ host: getFaucetHost(network), recipient: address, });
+
+    await sleep(10000);
+    console.log(`Current balance: ${parseBalance(await suiClient.getBalance({ owner: address }))}`)
+}
+
+async function transfer({ count, value, address, network }: { count: number, value: number, address: string, network: "mainnet" | "testnet" | "devnet" | "localnet" }) {
+    const keypair = fromBech32File(`./keys/${address}.key`);
+
+    console.log('Start sending from address: ', keypair.toSuiAddress())
+
+    const suiClient = new SuiClient({ url: getFullnodeUrl(network) });
+
+    console.log(`Current balance: ${parseBalance(await suiClient.getBalance({ owner: address }))}`)
+
+    for (let i = 0; i < count; i++) {
+        const pair = generatePairAndSave('temp');
+        console.log(`Sending ${value} SUI for address ${pair.toSuiAddress()} `)
+        const tx = createTxsendSUITo(pair.toSuiAddress(), value * Number(MIST_PER_SUI));
+
+        await suiClient.signAndExecuteTransaction({ signer: keypair, transaction: tx });
+    }
+
+    await sleep(3000);
+
+    console.log(`Current balance: ${parseBalance(await suiClient.getBalance({ owner: address }))}`)
+}
---
+++ src/utils.ts
@@ -0,0 +1,51 @@
+import fs from "fs";
+import { InvalidArgumentError } from "commander";
+import { decodeSuiPrivateKey } from "@mysten/sui/cryptography";
+import { Ed25519Keypair } from "@mysten/sui/keypairs/ed25519";
+import { Transaction } from '@mysten/sui/transactions';
+import { getFaucetHost, requestSuiFromFaucetV0 } from '@mysten/sui/faucet';
+import { MIST_PER_SUI } from '@mysten/sui/utils';
+import { CoinBalance } from "@mysten/sui/dist/cjs/client";
+
+export function myParseInt(value: string) {
+    const parsedValue = parseInt(value, 10);
+    if (isNaN(parsedValue)) {
+        throw new InvalidArgumentError('Not a number.');
+    }
+    return parsedValue;
+}
+
+export function fromBech32File(path: string) {
+    const privateStr = fs.readFileSync(path).toString();
+    const { secretKey } = decodeSuiPrivateKey(privateStr);
+    return Ed25519Keypair.fromSecretKey(secretKey);
+}
+
+export function generatePairAndSave(folder: string) {
+    const keypair = new Ed25519Keypair();
+
+    const privateKey = keypair.getSecretKey();
+    const publicKey = keypair.getPublicKey();
+
+    const address = publicKey.toSuiAddress();
+
+    fs.writeFileSync(`./${folder}/${address}.key`, privateKey);
+    fs.writeFileSync(`./${folder}/${address}.pub`, publicKey.toSuiPublicKey());
+
+    return keypair;
+}
+
+export function createTxsendSUITo(address: string, value: number) {
+    const tx = new Transaction();
+
+    const [coin] = tx.splitCoins(tx.gas, [value]);
+    tx.transferObjects([coin], address);
+
+    return tx;
+}
+
+export function parseBalance(balance: CoinBalance){
+	return Number.parseInt(balance.totalBalance) / Number(MIST_PER_SUI);
+};
+
+export const sleep = (delay: number) => new Promise((resolve) => setTimeout(resolve, delay))
